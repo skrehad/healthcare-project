7e4b822dffdddee7388d913f9bf79e8edcabe910
rehadhasan <rehadhasan664@gmail.com>
2025-04-13 00:04:35 +0600

    validation added

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { AdminController } from "./admin.controller";
+import { adminValidationSchemas } from "./admin.validations";
+import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
 router.get("/", AdminController.getAllFromDB);
 router.get("/:id", AdminController.getSingleIdFromDB);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch(
+  "/:id",
+  validateRequest(adminValidationSchemas.update),
+  AdminController.updateIntoDB
+);
 router.delete("/:id", AdminController.deleteFromDB);
 router.delete("/soft/:id", AdminController.softDeleteFromDB);
 
