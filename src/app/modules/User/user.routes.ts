f7a0836be3a5bf5b5443982ea5adad682bb8bd6d
rehadhasan <rehadhasan664@gmail.com>
2025-04-21 01:52:27 +0600

    api error handle

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { userController } from "./user.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "../../../generated/prisma";
 
 const router = express.Router();
 
-router.post("/", userController.createAdmin);
+router.post(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  userController.createAdmin
+);
 
 export const userRoutes = router;
