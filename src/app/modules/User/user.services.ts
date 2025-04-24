9f50e38f3c789aae550d00dae509971c59b2389d
rehadhasan <rehadhasan664@gmail.com>
2025-04-24 19:59:55 +0600

    get all admin

--- src/app/modules/User/user.services.ts
+++ src/app/modules/User/user.services.ts
@@ -105,6 +105,65 @@ const createPatient = async (req: Request): Promise<Patient> => {
   return result;
 };
 
+const getAllFromDB = async (params: any, options: any) => {
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
+  const andConditions: Prisma.AdminWhereInput[] = [];
+
+  //console.log(filterData);
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          //   for case sensitive
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+
+  //console.dir(andConditions, { depth: 'infinity' })
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
+
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+  });
+
+  const total = await prisma.admin.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const userService = {
   createAdmin,
   createDoctor,
