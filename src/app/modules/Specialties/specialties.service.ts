34a93ecf89bb38d952004d94ee9d330d95b13a87
rehadhasan <rehadhasan664@gmail.com>
2025-04-28 12:38:52 +0600

    Specialties Services added

---
+++ src/app/modules/Specialties/specialties.service.ts
@@ -0,0 +1,39 @@
+import { Request } from "express";
+import prisma from "../../../shared/prisma";
+import { IFile } from "../../interfaces/file";
+import { fileUploader } from "../../../helpers/fileUploader";
+import { Specialties } from "../../../generated/prisma";
+
+const insertIntoDB = async (req: Request) => {
+  const file = req.file as IFile;
+
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+    req.body.icon = uploadToCloudinary?.secure_url;
+  }
+
+  const result = await prisma.specialties.create({
+    data: req.body,
+  });
+
+  return result;
+};
+
+const getAllFromDB = async (): Promise<Specialties[]> => {
+  return await prisma.specialties.findMany();
+};
+
+const deleteFromDB = async (id: string): Promise<Specialties> => {
+  const result = await prisma.specialties.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+export const SpecialtiesService = {
+  insertIntoDB,
+  getAllFromDB,
+  deleteFromDB,
+};
