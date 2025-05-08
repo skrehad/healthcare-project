d15dadb12e9f9389a41093a2f80ba4dff5b56552
rehadhasan <rehadhasan664@gmail.com>
2025-05-08 20:53:27 +0600

    get All schedule controller

--- src/app/modules/Schedule/schedule.controller.ts
+++ src/app/modules/Schedule/schedule.controller.ts
@@ -1,181 +1,66 @@
-import { addHours, addMinutes, format } from "date-fns";
-import prisma from "../../../shared/prisma";
-import { IFilterRequest, ISchedule } from "./schedule.interface";
-import { IPaginationOptions } from "../../interfaces/pagination";
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import pick from "../../../shared/pick";
 import { IAuthUser } from "../../interfaces/common";
-import { Prisma, Schedule } from "../../../generated/prisma";
-import { paginationHelper } from "../../../helpers/paginationHelper";
+import { ScheduleService } from "./schedule.service";
 
-const insertIntoDB = async (payload: ISchedule): Promise<Schedule[]> => {
-  const { startDate, endDate, startTime, endTime } = payload;
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await ScheduleService.insertIntoDB(req.body);
 
-  const intervalTime = 30;
-
-  const schedules = [];
-
-  const currentDate = new Date(startDate); // start date
-  const lastDate = new Date(endDate); // end date
-
-  while (currentDate <= lastDate) {
-    // 09:30  ---> ['09', '30']
-    const startDateTime = new Date(
-      addMinutes(
-        addHours(
-          `${format(currentDate, "yyyy-MM-dd")}`,
-          Number(startTime.split(":")[0])
-        ),
-        Number(startTime.split(":")[1])
-      )
-    );
-
-    const endDateTime = new Date(
-      addMinutes(
-        addHours(
-          `${format(currentDate, "yyyy-MM-dd")}`,
-          Number(endTime.split(":")[0])
-        ),
-        Number(endTime.split(":")[1])
-      )
-    );
-
-    while (startDateTime < endDateTime) {
-      const scheduleData = {
-        startDateTime: startDateTime,
-        endDateTime: addMinutes(startDateTime, intervalTime),
-      };
-
-      const existingSchedule = await prisma.schedule.findFirst({
-        where: {
-          startDateTime: scheduleData.startDateTime,
-          endDateTime: scheduleData.endDateTime,
-        },
-      });
-
-      if (!existingSchedule) {
-        const result = await prisma.schedule.create({
-          data: scheduleData,
-        });
-        schedules.push(result);
-      }
-
-      startDateTime.setMinutes(startDateTime.getMinutes() + intervalTime);
-    }
-
-    currentDate.setDate(currentDate.getDate() + 1);
-  }
-
-  return schedules;
-};
-
-const getAllFromDB = async (
-  filters: IFilterRequest,
-  options: IPaginationOptions,
-  user: IAuthUser
-) => {
-  const { limit, page, skip } = paginationHelper.calculatePagination(options);
-  const { startDate, endDate, ...filterData } = filters;
-
-  const andConditions = [];
-
-  if (startDate && endDate) {
-    andConditions.push({
-      AND: [
-        {
-          startDateTime: {
-            gte: startDate,
-          },
-        },
-        {
-          endDateTime: {
-            lte: endDate,
-          },
-        },
-      ],
-    });
-  }
-
-  if (Object.keys(filterData).length > 0) {
-    andConditions.push({
-      AND: Object.keys(filterData).map((key) => {
-        return {
-          [key]: {
-            equals: (filterData as any)[key],
-          },
-        };
-      }),
-    });
-  }
-
-  const whereConditions: Prisma.ScheduleWhereInput =
-    andConditions.length > 0 ? { AND: andConditions } : {};
-
-  const doctorSchedules = await prisma.doctorSchedules.findMany({
-    where: {
-      doctor: {
-        email: user?.email,
-      },
-    },
-  });
-
-  const doctorScheduleIds = doctorSchedules.map(
-    (schedule) => schedule.scheduleId
-  );
-  console.log(doctorScheduleIds);
-
-  const result = await prisma.schedule.findMany({
-    where: {
-      ...whereConditions,
-      id: {
-        notIn: doctorScheduleIds,
-      },
-    },
-    skip,
-    take: limit,
-    orderBy:
-      options.sortBy && options.sortOrder
-        ? { [options.sortBy]: options.sortOrder }
-        : {
-            createdAt: "desc",
-          },
-  });
-  const total = await prisma.schedule.count({
-    where: {
-      ...whereConditions,
-      id: {
-        notIn: doctorScheduleIds,
-      },
-    },
-  });
-
-  return {
-    meta: {
-      total,
-      page,
-      limit,
-    },
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Schedule created successfully!",
     data: result,
-  };
-};
-
-const getByIdFromDB = async (id: string): Promise<Schedule | null> => {
-  const result = await prisma.schedule.findUnique({
-    where: {
-      id,
-    },
   });
-  return result;
-};
+});
 
-const deleteFromDB = async (id: string): Promise<Schedule> => {
-  const result = await prisma.schedule.delete({
-    where: {
-      id,
-    },
+const getAllFromDB = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const filters = pick(req.query, ["startDate", "endDate"]);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    const user = req.user;
+    const result = await ScheduleService.getAllFromDB(
+      filters,
+      options,
+      user as IAuthUser
+    );
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Schedule fetched successfully!",
+      data: result,
+    });
+  }
+);
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ScheduleService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Schedule retrieval successfully",
+    data: result,
   });
-  return result;
-};
+});
 
-export const ScheduleService = {
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ScheduleService.deleteFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Schedule deleted successfully",
+    data: result,
+  });
+});
+
+export const ScheduleController = {
   insertIntoDB,
   getAllFromDB,
   getByIdFromDB,
