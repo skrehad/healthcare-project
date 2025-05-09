38aa44d507fc6683871c98164c89cc22d1567e8b
rehadhasan <rehadhasan664@gmail.com>
2025-05-09 20:55:12 +0600

    schedule  route change

--- src/app/modules/Schedule/schedule.routes.ts
+++ src/app/modules/Schedule/schedule.routes.ts
@@ -1,8 +1,39 @@
 import express from "express";
 import { ScheduleController } from "./schedule.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "../../../generated/prisma";
 
 const router = express.Router();
 
-router.post("/", ScheduleController.insertIntoDB);
+router.get("/", auth(UserRole.DOCTOR), ScheduleController.getAllFromDB);
+
+/**
+ * API ENDPOINT: /schedule/:id
+ *
+ * Get schedule data by id
+ */
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+  ScheduleController.getByIdFromDB
+);
+
+router.post(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  ScheduleController.insertIntoDB
+);
+
+/**
+ * API ENDPOINT: /schedule/:id
+ *
+ * Delete schedule data by id
+ */
+
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  ScheduleController.deleteFromDB
+);
 
 export const ScheduleRoutes = router;
